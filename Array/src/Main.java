5b0004115a94d4738168e7d18db5575fd70b23f6
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2022-12-18 21:45:30 +0530

    Java Array
    This is Java Array

---
+++ Array/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number [] = {1,2,3,4};
+        String names [] = {"JD","Master","Jeewantha","Dilshan"};
+
+
+        System.out.println("This is Number Array: "+number[1]);
+        System.out.println("This is Number Array: "+number[2]);
+        System.out.println("This is Number Array: "+number[0]);
+
+        System.out.println("This is String Array: "+names[1]);
+        System.out.println("This is String Array: "+names[2]);
+        System.out.println("This is String Array: "+names[3]);
+
+
+    }
+}
