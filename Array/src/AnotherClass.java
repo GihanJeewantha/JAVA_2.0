a4777d98b23822af7c2ec2f78ccdefad3a43ba4e
Jeewantha Dilshan <106438829+GihanJeewantha@users.noreply.github.com>
2022-12-18 21:50:29 +0530

    Create Another Array
    Using Object

---
+++ Array/src/AnotherClass.java
@@ -0,0 +1,15 @@
+public class AnotherClass {
+
+    public static void main(String[]args){
+        String [] names = new String[5];
+
+        names[0] = "Tharushi";
+        names[1] = "Master JD";
+        names[4] = "This is another Name";
+
+        System.out.println(names[4]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+
+    }
+}
